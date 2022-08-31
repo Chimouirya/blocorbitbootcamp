d9350f3f8edb44765863c6992a16af2df2d0c920
unknown <chimouriyashyam@gmail.com>
2022-08-31 12:09:43 +0545

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Mainnav from "./component/Mainnav";
+import Mainnav from "./component/Mainnav"; 
 
 export default function Home() {
   return (
    
    <div>
-   
+   <NewNav/>
     <Mainnav/>
     { <h1 className="text-3xl font-bold underline">
       Hello world!
