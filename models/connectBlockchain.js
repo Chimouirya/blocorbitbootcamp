1518e418535c481513c81b62125fbab629aefb5f
BlocOrbit <manishthapa605@gmail.com>
2022-09-11 16:47:55 +0545

    upload final product

--- models/connectBlockchain.js
+++ models/connectBlockchain.js
@@ -1,10 +1,11 @@
 const { ethers } = require("ethers");
+import Web3Modal from "web3modal";
 const connectBlockchain=()=>{
-    // const etherScan = new ethers.providers.EtherscanProvider('maticmum','CJWX9VHAEBX1JGQFXMN1ZWF3BBH3P8GBQ3');
+    const etherScan = new ethers.providers.AlchemyProvider("maticmum","jInQMmu-mUanH_Z09I_MStfWl02IpTc9")
     const abi = '[{"inputs":[],"name":"getUserName","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"newUserName","type":"string"}],"name":"setUserName","outputs":[],"stateMutability":"nonpayable","type":"function"}]';
 
-    // const userWallet = new ethers.Wallet('3e8bbb2e8e85be328b5e744434b0e524504f7ad83e0df97fbcd2bb7dd8a43a3e',etherScan);
-    const connectContact = new ethers.Contract('0x53343a0D223627C5cD672987C669d0Aa3Da07480',abi);
+    const userWallet = new ethers.Wallet('3e8bbb2e8e85be328b5e744434b0e524504f7ad83e0df97fbcd2bb7dd8a43a3e',etherScan);
+    const connectContact = new ethers.Contract('0x53343a0D223627C5cD672987C669d0Aa3Da07480',abi,userWallet);
     return connectContact;
 }
 export default connectBlockchain;
--- pages/api/blockchainapi.js
+++ pages/api/blockchainapi.js
@@ -1,7 +1,7 @@
 import connectBlockchain from "../../models/connectBlockchain"
 export default function handler(req, res) {
-    connectBlockchain().getUserName().then(()=>{
-        res.status(200).json({ name: 'ok' })
+    connectBlockchain().getUserName().then((response)=>{
+        res.status(200).json({ name: response })
     }).catch((error)=>{
         res.status(301).json({ name: 'Error=>'+error })
     })
